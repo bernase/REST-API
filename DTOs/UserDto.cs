d7d0fcac73ccf0029a1eda1395db0c0778354818
Berna <93603466+bernase@users.noreply.github.com>
2024-06-06 19:42:45 +0200

    Swagger Config

--- DTOs/UserDto.cs
+++ DTOs/UserDto.cs
@@ -2,7 +2,7 @@ namespace REST_API.DTOs
 {
     public class UserDto
 {
-    public string Username { get; set; }
+    public string UserName { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
 }
